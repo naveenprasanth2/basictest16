cc30892f5394005a2781794d157483fd20ae7a0b
Naveen Prasanth Ramasamy <naveenprasanth12@gmail.com>
2025-07-23 08:13:17 +0530

    Functional programming implementation

--- src/main/java/com/symphony/practise2/FunctionalTest.java
+++ src/main/java/com/symphony/practise2/FunctionalTest.java
@@ -1,13 +1,18 @@
 package com.symphony.practise2;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 public class FunctionalTest {
     public static void main(String[] args) {
 
         List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
+        Consumer<Integer> consumer = x -> System.out.println(x * 10);
+
         List<Integer> result = list.stream().map(x -> x * 10).filter(x -> x % 3 == 0).toList();
+
+        list.stream().map(x -> x * 10).filter(x -> x % 3 == 0).forEach(consumer);
 
         System.out.println(result);
 
