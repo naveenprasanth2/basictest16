7a482c83c4147875202e1b5cba7742e7ed6a472d
Naveen Prasanth Ramasamy <naveenprasanth12@gmail.com>
2025-09-21 11:25:25 +0530

    converting arrays to list for characters

--- src/main/java/com/symphony/practise1/FindSentence.java
+++ src/main/java/com/symphony/practise1/FindSentence.java
@@ -1,5 +1,6 @@
 package com.symphony.practise1;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
@@ -10,5 +11,14 @@ public class FindSentence {
         List<String> result = Stream.of(sentence.split(" "))
                 .filter(x -> Pattern.matches("[a-zA-Z]+|[a-zA-Z]+-?[a-zA-Z]+", x)).toList();
         System.out.println(result);
+        long sum = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9).mapToInt(Integer::intValue).sum();
+        System.out.println(sum);
+
+        int[] arr = {1,2,3,4,5,6,7};
+        Arrays.stream(arr).filter(x -> x%2==0).forEach(System.out::println);
+
+        char[] chars = {'a','b','c','d'};
+        List<Character> charList = new String(chars).chars().mapToObj(c -> (char) c).toList();
+        System.out.println(charList);
     }
 }
